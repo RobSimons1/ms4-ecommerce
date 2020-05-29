374ce328508cba93f2e49b8f5018b5a24fe05748
RobSimons1 <rob.simons79@gmail.com>
2020-05-29 21:55:11 +0000

    Added product.info to models and product.html

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
+    info = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 
