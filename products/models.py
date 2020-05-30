624f22f57426d4cc8ff7cd9fff80cf282ea6d28f
RobSimons1 <rob.simons79@gmail.com>
2020-05-30 18:18:31 +0000

    blurb section added to products page. Botstrap grid altered to accomodate this

--- products/models.py
+++ products/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
-    description = models.TextField()
-    info = models.TextField()
+    description = models.CharField(max_length=254, default='')
+    info = models.TextField(default='')
+    blurb = models.TextField(default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 
