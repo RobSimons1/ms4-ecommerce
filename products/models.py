fa0ea605d0cc893a232a50eaf0bc91927377b4af
RobSimons1 <rob.simons79@gmail.com>
2020-06-04 14:10:05 +0000

    Alcohol content added to product page and Django Admin

--- products/models.py
+++ products/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.CharField(max_length=254, default='')
-    info = models.TextField(default='')
+    info = models.CharField(max_length=500, default='')
     blurb = models.TextField(default='')
+    size = models.CharField(max_length=254, default='')
+    alcohol = models.CharField(max_length=254, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 
