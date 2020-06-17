ac0ff213881f97618e006b429dba0e2d844862c5
RobSimons1 <rob.simons79@gmail.com>
2020-06-17 11:28:24 +0000

    contact form moved to accounts app, as shares same attributes as registration form

--- contact/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def contact(request):
-    """A view that diplays the contact page"""
-    return render(request, "contact.html")
-
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
-    'contact',
+    
 ]
 
 MIDDLEWARE = [
