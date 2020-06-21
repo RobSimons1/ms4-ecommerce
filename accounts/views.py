bb4ade85b1a6982533c384f89f82e21f4f90001d
RobSimons1 <rob.simons79@gmail.com>
2020-06-21 20:22:00 +0000

    ecommerce  urls.py trying to configure to allow email from contact frm to be sent

--- accounts/views.py
+++ accounts/views.py
@@ -33,7 +33,7 @@ def contact(request):
                 fail_silently=False,
             )
 
-            return HttpResponseRedirect('/accounts/') # Redirect after POST
+            return HttpResponseRedirect('/accounts/contact/') # Redirect after POST
     else:
         user_form = ContactForm()  # An unbound form
 
