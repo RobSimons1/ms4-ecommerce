cc7a22e6297c47c3bf2e0c3d275c880d4d111d81
RobSimons1 <rob.simons79@gmail.com>
2020-07-16 19:27:14 +0000

    further work on Ratings model

--- user_ratings/views.py
+++ user_ratings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Item
+from django.contrib import messages
 
 
 def create_an_item(request):
@@ -8,6 +9,7 @@ def create_an_item(request):
         new_item.name = request.POST.get('name')
         new_item.done = 'done' in request.POST
         new_item.save()
+        messages.success(request, 'Thankyou for your review')
 
         return redirect(create_an_item)
 
