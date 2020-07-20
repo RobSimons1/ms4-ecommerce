020573461d8a27945b004689da33ee8bdc8bd96a
RobSimons1 <rob.simons79@gmail.com>
2020-07-20 14:56:55 +0000

    Rating scale added to user_ratings

--- user_ratings/models.py
+++ user_ratings/models.py
@@ -7,6 +7,16 @@ class Item(models.Model):
     name = models.CharField(max_length=200, blank=False)
     beer = models.CharField(max_length=500, default='', blank=False)
     review = models.CharField(max_length=2000, default='', blank=False)
+    RATING_CHOICES = (
+        ("1", '1'),
+        ("2", '2'),
+        ("3", '3'),
+        ("4", '4'),
+        ("5", '5'),
+    )
+    rating = models.CharField(max_length=10,
+                                      choices=RATING_CHOICES,
+                                      default="3")
     done = models.BooleanField(blank=False, default=False)
 
     def __str__(self):
--- user_ratings/urls.py
+++ user_ratings/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import get_review_list, create_an_item
 
 urlpatterns = [
-    url(r'^get_review_list', get_review_list),
-    url(r'^add$', create_an_item), # user_ratings urls import
+    url(r'^get_review_list/$', get_review_list),
+    url(r'^add/$', create_an_item), # user_ratings urls import
 ] 
