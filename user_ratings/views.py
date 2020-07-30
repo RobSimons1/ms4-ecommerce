313782d79da2e045f99fc6d4b7a7c0853789676e
RobSimons1 <rob.simons79@gmail.com>
2020-07-30 19:10:29 +0000

    Filter product=prduct added to get_review_list function that shows specific reviews for each product

--- user_ratings/views.py
+++ user_ratings/views.py
@@ -9,16 +9,16 @@ from datetime import datetime
 
 def get_review_list(request, id):
     product = get_object_or_404(Product, pk=id)
-    review = Review.objects.filter(date__lte=datetime.now()).order_by('-date')
+    reviews = Review.objects.all().filter(product=product)
     return render(request, "user_reviews.html", {
         'product': product,
-        'review': review,
-        
+        'items': reviews
+
     })    
 
 def create_an_item(request, id):
     product = get_object_or_404(Product, pk=id)
-    
+    reviews = Review.objects.all().filter(product=product)
     if request.method == "POST":
         new_item = ItemForm(request.POST) # post the new_item to the ItemForm
         if new_item.is_valid():
@@ -26,16 +26,15 @@ def create_an_item(request, id):
             new_item.instance.name = request.POST.get('name') # instance passes keyword argument to the model whose relations will be edited in the formset
             new_item.instance.review = request.POST.get('review')
             new_item.instance.rating = request.POST.get('rating')
-            new_item.save()            
-
+            
+            new_item.save()
         messages.success(request, 'Thankyou for your review')
 
         return render(request, "user_reviews.html", {
-        'new_item': new_item,    
+        'new_item': new_item,
         'product': product,
-        
-
+        'items': reviews
         
     })
 
-    return render(request, "user_ratings.html", {'product': product}) # Pass through context in GET method to display form. 
+    return render(request, "user_ratings.html", {'product': product, 'items': reviews}) # Pass through context in GET method to display form. 
