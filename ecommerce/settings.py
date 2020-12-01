e9b3a0bd62cf845dfbaf3764e6a9c1204b4fc2dd
RobSimons1 <rob.simons79@gmail.com>
2020-12-01 12:09:13 +0000

    Reverted media file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -164,10 +164,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-MEDIAFILES_LOCATION = 'media/images/'
+MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
